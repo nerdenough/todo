c233561dd51c8fce6fea0e4afe2a78237489c695
Brendan Goodenough <brendan@goodenough.nz>
2017-03-14 20:44:06 +1300

    Add todo header

---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="todo-header">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'todo-header',
+  components: {
+  }
+};
+</script>
+
+<style scoped>
+.todo-header {
+  width: 100%;
+  height: 80px;
+  background: #8BC34A;
+}
+</style>
