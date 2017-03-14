83188799aa74291d22be5e2d8f6aa81bbe24bc79
Brendan Goodenough <brendan@goodenough.nz>
2017-03-14 21:15:51 +1300

    Add border radius to the text field

--- src/components/TextField.vue
+++ src/components/TextField.vue
@@ -21,6 +21,8 @@ export default {
   outline: none;
   padding: 0 15px;
   box-sizing: border-box;
+  border-top-left-radius: 10px;
+  border-bottom-left-radius: 10px;
 }
 
 .text-field::placeholder {
